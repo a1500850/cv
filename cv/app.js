cc8d1977b554bc52757ea4fc00aec29bdebf9a2b
Joonas Mattila <joonasunal@gmail.com>
2017-08-25 13:41:52 +0300

    express error handling redirects 404 to index

--- cv/app.js
+++ cv/app.js
@@ -38,8 +38,12 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  if(err.status === 404){
+    res.redirect('/');
+  }else{
+    res.status(err.status || 500);
+    res.render('error');
+  }
 });
 
 module.exports = app;
